b0a9a6a60cb03ebc28a6c94eb4a1e6ecb0b6f5be
Ayyappa <nayyappa@deloitte.com>
2019-08-07 23:09:37 +0530

    Changes css config to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'angular-poc';
