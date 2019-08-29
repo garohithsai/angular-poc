06dc3758cc6ae278aedbb10d8e026e8c66f51c86
Ayyappa <nayyappa@deloitte.com>
2019-08-29 15:07:32 +0530

    commenting spec files

--- src/app/business/form-review/components/form-guideline-banner/form-guideline-banner.component.spec.ts
+++ src/app/business/form-review/components/form-guideline-banner/form-guideline-banner.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FormGuidelineBannerComponent } from './form-guideline-banner.component';
+// import { FormGuidelineBannerComponent } from './form-guideline-banner.component';
 
-describe('FormGuidelineBannerComponent', () => {
-  let component: FormGuidelineBannerComponent;
-  let fixture: ComponentFixture<FormGuidelineBannerComponent>;
+// describe('FormGuidelineBannerComponent', () => {
+//   let component: FormGuidelineBannerComponent;
+//   let fixture: ComponentFixture<FormGuidelineBannerComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FormGuidelineBannerComponent ]
-    })
-    .compileComponents();
-  }));
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ FormGuidelineBannerComponent ]
+//     })
+//     .compileComponents();
+//   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FormGuidelineBannerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(FormGuidelineBannerComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
